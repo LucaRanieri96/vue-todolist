6233e12b9093435e85f2976dbfd441ea91a6ff0f
LucaRanieri96 <luca.ranieri.4@gmail.com>
2023-03-30 16:51:10 +0200

    Add barred class to list item

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,15 +1,31 @@
-
 const { createApp } = Vue;
 
 createApp({
   data() {
     return {
-     message:"ciao"
+      title: "TODO LIST",
+      tasks: [
+        { text: "Learn Html", 
+          done: false
+        },
+        { text: "Learn css", 
+          done: false 
+        },
+        { text: "Learn php", 
+          done: false 
+        },
+        { text: "Learn javascript", 
+          done: false 
+        },
+      ],
     };
   },
   methods: {
-    
-}
-}).mount('#app')
+    barredTask(i) {
+      this.tasks[i].done = true;
+      console.log("hai cliccato sull'icona");
+    }
+  },
+}).mount("#app");
 
 // add event listener v-on @ sul button
